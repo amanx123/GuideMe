2a6629c198364c0874576bdc2e5878b36f6c137d
amanx123 <amanagarwalx123@gmail.com>
2021-09-05 00:14:42 +0530

    Updates

---
+++ precache-manifest.b93a739ce94464a35be2c6dfc02746aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19018b7e40c85b1b4ba1f08b7e0b29a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc0979bf4926d6680a5e",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "29e4978967a837c89678",
+    "url": "/static/js/2.607ebf82.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.607ebf82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc0979bf4926d6680a5e",
+    "url": "/static/js/main.c8a0451e.chunk.js"
+  },
+  {
+    "revision": "2381cc2a2fab06c5c881",
+    "url": "/static/js/runtime-main.c4946b14.js"
+  },
+  {
+    "revision": "2fdd48bd56d79234dc26dcf81b1e56a2",
+    "url": "/static/media/amanagarwal.2fdd48bd.jpg"
+  }
+]);
